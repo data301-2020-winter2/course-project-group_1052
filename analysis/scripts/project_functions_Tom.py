b2688b35b585cc9ad2ed2acc0709e95b9a376da2
Tom Head <thomasmhead@gmail.com>
2021-03-13 13:30:29 -0800

    began work on MS2T4 making progress on EDA

--- analysis/scripts/project_functions_Tom.py
+++ analysis/scripts/project_functions_Tom.py
@@ -9,3 +9,18 @@ def load_and_process(path):
             .sort_values("year", ascending = True)
     )
     return df2
+
+def badword_count(dataframe):
+    df1 = (pd.DataFrame(dataframe['badWord']
+                  .value_counts())
+                  .reset_index()
+                  .rename(columns={'index':'badWord','badWord':'frequency'})
+                 )
+    return df1
+
+def unique_word_count(dataframe):
+    df1 = (dataframe.groupby('ogArtist')['badWord']
+             .nunique()
+             .sort_values(ascending = False))
+    
+    return df1
